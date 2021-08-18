c6ce748e67de0139c8c77c94b0571df33b964fc9
tonipone <terseo62@gmail.com>
2021-08-18 15:26:36 +0200

    V6

--- index.php
+++ index.php
@@ -6,5 +6,17 @@ use \Core\Config as config;
 define('PROOT',__DIR__);
 define('DS',DIRECTORY_SEPARATOR);
 //define('ROOT',dirname(__FILE__));
-$ver = config::get('version');
-var_dump(PROOT);
+
+spl_autoload_register(function($className){
+    $parts = explode('\\', $className);
+    $class = end($parts);
+    array_pop($parts);
+    $path = strtolower(implode(DS, $parts));
+    $path = PROOT . DS . $path . DS . $class . '.php';
+    if(file_exists($path)) {
+        include($path);
+    }
+});
+
+$dbName = Config::get('db_name');
+var_dump($dbName);
