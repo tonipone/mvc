f5fd65f745208aba7258f8c8513347163a28c989
tonipone <terseo62@gmail.com>
2021-08-18 15:30:52 +0200

    V6

--- index.php
+++ index.php
@@ -8,5 +8,16 @@ define('PROOT',__DIR__);
 define('DS',DIRECTORY_SEPARATOR);
 include('core/Config.php');
 //define('ROOT',dirname(__FILE__));
-$ver = Config::get('version');
-var_dump($ver);
+spl_autoload_register(function($className){
+    $parts = explode('\\', $className);
+    $class = end($parts);
+    array_pop($parts);
+    $path = strtolower(implode(DS, $parts));
+    $path = PROOT . DS . $path . DS . $class . '.php';
+    if(file_exists($path)) {
+        include($path);
+    }
+});
+
+$dbName = Config::get('db_name');
+var_dump($dbName);
