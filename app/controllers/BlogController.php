2bfdd005b179117324ba187a65402aaebc3c3958
tonipone <terseo62@gmail.com>
2021-08-24 09:01:44 +0200

    Lez. 11 Output Buffer 24Ago

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -7,6 +7,7 @@ use Core\Controller;
 class BlogController extends Controller {
 
 	public function indexAction(){
+		$this->view->setSiteTitle('Blog');
 		$this->view->render();
 	}
 
--- app/views/blog/index.php
+++ app/views/blog/index.php
@@ -1,1 +1,7 @@
+<?php $this->start('head'); ?>
+    <script>alert("head content loaded")</script>
+<?php $this->end(); ?>
+
+<?php $this->start('content'); ?>
 <h1>Welcome to our Blog</h1>
+<?php $this->end(); ?>
