fe752166454ebece877e8790ca73c182f63107cb
tonipone <terseo62@gmail.com>
2021-08-26 14:27:45 +0200

    Lez.13 DB Connection

---
+++ core/H.php
@@ -0,0 +1,16 @@
+<?php
+namespace Core;
+
+class H
+{
+	
+	public static function dnd($data=[],$die =true){
+		
+		echo "<pre>";
+		var_dump($data);
+		echo "</pre>";
+		if($die){
+			die;
+		}
+	} 
+}
