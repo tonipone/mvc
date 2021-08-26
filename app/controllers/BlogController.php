09bb36036d6d55a3e488a029502d1aab3e0ba628
tonipone <terseo62@gmail.com>
2021-08-26 14:43:20 +0200

    Lez.13 DB Connection

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -8,7 +8,7 @@ class BlogController extends Controller {
 
 	public function indexAction(){
 		$db = DB::getInstance();
-		H::dnd($db,false);
+		//H::dnd($db,false);
 		$this->view->setSiteTitle('Blog');
 		$this->view->render();
 	}
