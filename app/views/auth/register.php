69622443bb542917f3e9e9cb418ccc8101f2b352
tonipone <terseo62@gmail.com>
2021-08-31 16:19:12 +0200

    Lez.19 Register Form 31AGO21

---
+++ app/views/auth/register.php
@@ -0,0 +1,28 @@
+<?php use Core\FH;?>
+<?php $this->start('head'); ?>
+<?php $this->end(); ?>
+
+<?php $this->start('content'); ?>
+<div class="row">
+	<div class="col-md-8 offset-md-2 poster">
+		<h1>Register Users</h1>
+		
+		
+		<form action="" method="POST">
+			<div class="row">
+				<?= FH::inputBlock('First Name', 'fname', '', ['class' => 'form-control'], ['class' => 'form-group col-md-6'], $this->errors ); ?>
+				<?= FH::inputBlock('Last Name', 'lname', '', ['class' => 'form-control'], ['class' => 'form-group col-md-6'], $this->errors ); ?>				
+				<?= FH::inputBlock('Email', 'email', '', ['class' => 'form-control', 'type' => 'email'], ['class' => 'form-group col-md-6'], $this->errors ); ?>			
+			</div>
+			<div class="row">
+				<?= FH::inputBlock('Password', 'password', '', ['class' => 'form-control','type'=>'password'], ['class' => 'form-group col-md-6'], $this->errors ); ?>
+				<?= FH::inputBlock('Confirm Password', 'confirm', '', ['class' => 'form-control','type'=>'password'], ['class' => 'form-group col-md-6'], $this->errors ); ?>				
+			</div>
+			<div class="text-right">
+				<a href="#" class="btn btn-secondary">Cancel</a>
+				<input class="btn btn-primary" value="Save" type="submit" /> 
+			</div>
+		</form>
+	</div>
+</div
+<?php $this->end(); ?>
--- core/Router.php
+++ core/Router.php
@@ -14,9 +14,11 @@ class Router {
 		// set controller
 		$controller = !empty($urlParts[0])? $urlParts[0] : Config::get('default_controller');
 		$controllerName = $controller;
+		
+		
 		$controller = '\App\Controllers\\' . ucwords($controller). 'Controller';
 
-
+		
 		//set action
 		array_shift($urlParts);
 		$action = !empty($urlParts[0])? $urlParts[0] : 'index';
