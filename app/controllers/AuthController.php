c5b79efbcd4fb6c9190af8e951beab83a00999e6
tonipone <terseo62@gmail.com>
2021-09-01 15:42:39 +0200

    Lez.20 register Form part-2 01Sett21

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -3,11 +3,27 @@
 namespace App\Controllers;
 
 use Core\{Controller, H, Session, Router};
+use App\Models\Users;
 
 class AuthController extends Controller{
 	
 	public function registerAction($id='new'){
-		$this->view->errors = ['fname' => 'First name is required'];
+		if($id == 'new'){
+			$user = new Users();
+			//var_dump($user);
+		}else{
+			$user = Users::findById($id);
+		}
+		
+		// if posted
+		if($this->request->isPost()){
+			H::dnd($this->request->get());
+		}
+		
+		$this->view->user = $user;		
+		//$this->view->errors = ['fname' => 'First name is required'];
+		$this->view->role_options = ['' => '', Users::AUTHOR_PERMISSION => 'Author', Users::ADMIN_PERMISSION => 'Admin'];
+		$this->view->errors = $user->getErrors();
 		$this->view->render();
 	}
 }
---
+++ app/models/Users.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Core\Model;
+
+class Users extends Model {
+	
+	protected static $table = "users";
+	public $id, $created_at, $updated_at, $fname, $lname, $email, $password, $acl, $blocked=0, $confirm, $remember='';
+	
+	
+	const AUTHOR_PERMISSION = 'author';
+	const ADMIN_PERMISSION = 'admin';
+}
